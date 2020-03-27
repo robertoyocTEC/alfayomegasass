505d4fba583179049d3a4930ee9a2e6aaae00e2e
Roberto Yoc <A01172599@itesm.mx>
2020-03-27 12:13:03 -0600

    added chat page

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('dist'));
+
+
+
+
+app.listen(4000, function () {
+    console.log('Example app listening on port 4000!');
+});
